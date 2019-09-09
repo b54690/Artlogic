a697468b3e4ce47003d326e4e519d6b4d70cd618
b54690 <belafonte@hotmail.co.uk>
2019-09-10 00:33:08 +0100

    break points added to container and typography

--- scripts/iterateArray.js
+++ scripts/iterateArray.js
@@ -2,7 +2,7 @@ var resolveList = function () {
     const questionList = document.querySelector('#toggle-list');
 
     questionList.innerHTML = '<ol class="toggle-list">' + window.questions.rows.map(function (row) {
-        return  '<li class="u-margin-bottom-small u-margin-top-small" id="expandable">' + row.title +
+        return  '<li class="u-margin-bottom-small u-margin-top-small" id="expandable">' + row.title     +
                 '<span class="icon__float-right"><button aria-expanded="false"></button></span>' +
                 '</li>' + '<p class="u-margin-bottom-medium" hidden>' + row.content + '</p>' +
                 '<hr class="line-break">';
