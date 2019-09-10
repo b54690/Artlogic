e06da1e23bfe87bdb0c0f93787037fadd6cd394d
b54690 <belafonte@hotmail.co.uk>
2019-09-10 10:52:11 +0100

    final checks done

--- scripts/iterateArray.js
+++ scripts/iterateArray.js
@@ -1,12 +1,15 @@
 var resolveList = function () {
     const questionList = document.querySelector('#toggle-list');
 
-    questionList.innerHTML = '<ol class="toggle-list">' + window.questions.rows.map(function (row) {
-        return  '<li class="u-margin-bottom-small u-margin-top-small" id="expandable">' + row.title     +
-                '<span class="icon__float-right"><button aria-expanded="false"></button></span>' +
-                '</li>' + '<p class="u-margin-bottom-medium" hidden>' + row.content + '</p>' +
-                '<hr class="line-break">';
-    }).join('') + '</ol>';
+    questionList.innerHTML =
+            '<ol class="toggle-list">' + window.questions.rows.map(function (row) {
+        return  '<li class="u-margin-bottom-small u-margin-top-small">' + row.title +
+                    '<button class="icon__float-right" aria-expanded="false"></button>' +
+                '</li>' +
+                '<p class="u-margin-bottom-medium" hidden>' + row.content +
+                '</p>' +
+                '<hr class="list__line-break">';
+}).join('') + '</ol>';
 };
 
 window.addEventListener('load', resolveList);
