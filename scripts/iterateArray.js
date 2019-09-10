5a7dd88736d4f52cb323492f825e16d781b2ab34
b54690 <belafonte@hotmail.co.uk>
2019-09-10 22:50:10 +0100

    refactored

--- scripts/iterateArray.js
+++ scripts/iterateArray.js
@@ -1,12 +1,12 @@
 var resolveList = function () {
-    const questionList = document.querySelector('#toggle-list');
+    var questionList = document.querySelector('#toggle-list');
 
     questionList.innerHTML =
-            '<ol class="toggle-list">' + window.questions.rows.map(function (row) {
-        return  '<li class="u-margin-bottom-small u-margin-top-small">' + row.title +
+               '<ol class="toggle-list">' + window.questions.rows.map(function (question) {
+        return  '<li class="u-margin-bottom-small u-margin-top-small">' + question.title +
                     '<button class="icon__float-right" aria-expanded="false"></button>' +
                 '</li>' +
-                '<p class="u-margin-bottom-medium" hidden>' + row.content +
+                '<p class="u-margin-bottom-medium" hidden>' + question.content +
                 '</p>' +
                 '<hr class="list__line-break">';
 }).join('') + '</ol>';
